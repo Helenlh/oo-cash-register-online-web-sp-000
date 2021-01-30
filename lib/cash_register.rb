3014f5c4b9ba9130a4537fca6f8f0f2c580176fc
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-30 23:46:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,27 +1,35 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, 
   
   def initialize(total = 0, discount = 20)
-    @total = total 
-    @discount = discount 
+    @total =  total 
+    @discount = discount
+    #@total = 0 
+    #@discount = 20 
   end 
  
   
   def total
-    puts total 
+    new_total = total + price 
+    new_total
   end
   
-  def add_item(item, price)
+  def add_item
     
   end 
   
   def apply_discount 
-    
+    if discount = 0 
+      puts "There is no discount to apply."
+    else 
+      puts "After the discount, the total comes to #{total - discount/100} "
   end 
-  
 end 
 
-
-  
+def items(item, price)
+  items = []
+  items << item 
+end
+end 
