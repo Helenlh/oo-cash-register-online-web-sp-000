d793da73bcefc08d829bea98f4244ed56908d5f9
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-30 00:53:05 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount = 20)
+    @total = total 
+    @discount = discount 
+  end 
+ 
+  
+  def total
+    puts total 
+  end
+  
+  def add_item(item, price)
+    
+  end 
+  
+  def apply_discount 
+    
+  end 
+  
+end 
+
+
+  
